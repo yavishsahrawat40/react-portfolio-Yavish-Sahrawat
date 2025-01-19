21bad9b86a0f82b90342fb3feb3d5dc5d79dd2ed
Yavish Sahrawat <115349811+yavishsahrawat40@users.noreply.github.com>
2025-01-19 23:44:49 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,40 +7,9 @@ export const HERO_CONTENT = `I am a skilled Full Stack Developer with expertise 
 
 export const ABOUT_TEXT = `I am a dedicated Full Stack Developer with expertise in the MERN stack (MongoDB, Express.js, React, and Node.js), complemented by an industrial certification in Full Stack Development from SmartInternz. I have a strong foundation in creating scalable, dynamic, and user-friendly web applications, with a focus on both front-end and back-end development. My skills include designing robust databases, building RESTful APIs, and developing responsive interfaces that enhance user experiences. Passionate about technology and continuous learning, I thrive on solving complex problems and contributing to impactful projects that make a difference.`;
 
-// export const EXPERIENCES = [
-//   {
-//     year: "2023 - Present",
-//     role: "Senior Full Stack Developer",
-//     company: "Google Inc.",
-//     description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-//     technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-//   },
-//   {
-//     year: "2022 - 2023",
-//     role: "Frontend Developer",
-//     company: "Adobe",
-//     description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-//     technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-//   },
-//   {
-//     year: "2021 - 2022",
-//     role: "Full Stack Developer",
-//     company: "Facebook",
-//     description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-//     technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-//   },
-//   {
-//     year: "2020 - 2021",
-//     role: "Software Engineer",
-//     company: "Paypal",
-//     description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-//     technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-//   },
-// ];
-
 export const PROJECTS = [
   {
-    title: "Spotify Cone",
+    title: "Spotify Clone",
     image: project1,
     description:
       "The Spotify Clone is a web-based application built using HTML, CSS, and JavaScript, designed to mimic the user interface and functionality of the popular music streaming platform.",
